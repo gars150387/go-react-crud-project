a8979ce632477cc286da44df34dbb7440c97d26d
Gustavo <grodriguez@contextglobal.com>
2025-03-02 00:32:42 -0500

    adding display function with event is mounted and after new item is created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { connecUrl } from "./api";
 
 function App() {
@@ -6,29 +6,31 @@ function App() {
   const [allUsers, setAllUsers] = useState([]);
   const handleButtonClick = async (e) => {
     e.preventDefault();
-    // axios
-    //   .get("https://go-react-crud-app-29a97a0eb165.herokuapp.com/users")
-    //   .then((response) => {
-    //     console.log(response.data);
-    //   });
     const response = await connecUrl.post("/user", {
       name: first,
     });
     if (response.data) {
-      console.log(response.data);
-      console.log(response.data.data.InsertedID);
       alert(`${response.data.data.InsertedID}`);
-      return setfirst("");
+      setfirst("");
+      return handleAllUsers();
     }
   };
 
   const handleAllUsers = async () => {
     const response = await connecUrl.get("/users");
     if (response.data) {
-      console.log(response.data);
       setAllUsers(response.data.data);
     }
   };
+
+  useEffect(() => {
+    const controller = new AbortController();
+    handleAllUsers();
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
   return (
     <div>
       <form onSubmit={(e) => handleButtonClick(e)}>
@@ -41,10 +43,9 @@ function App() {
       </form>
       <button onClick={() => handleAllUsers()}>All users</button>
       <ul>
-        {allUsers.map((user) => (
-          console.log(user),
-          <div key={user.InsertedID}>{user.name}</div>
-        ))}
+        {allUsers.map(
+          (user) => (console.log(user), (<div key={user._id}>{user.name}</div>))
+        )}
       </ul>
     </div>
   );
