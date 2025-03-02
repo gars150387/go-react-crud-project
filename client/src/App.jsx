69cd1584f3ed9bb57aaf3045b01982d4125c8b92
Gustavo <grodriguez@contextglobal.com>
2025-03-02 00:26:07 -0500

    adding routes/endpoint for http methods

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,52 @@
-import axios from 'axios';
+import { useState } from "react";
+import { connecUrl } from "./api";
 
 function App() {
-  const handleButtonClick = () => {
-    axios.get('https://go-react-crud-app-29a97a0eb165.herokuapp.com/users').then((response) => {
-      console.log(response.data);
+  const [first, setfirst] = useState("");
+  const [allUsers, setAllUsers] = useState([]);
+  const handleButtonClick = async (e) => {
+    e.preventDefault();
+    // axios
+    //   .get("https://go-react-crud-app-29a97a0eb165.herokuapp.com/users")
+    //   .then((response) => {
+    //     console.log(response.data);
+    //   });
+    const response = await connecUrl.post("/user", {
+      name: first,
     });
+    if (response.data) {
+      console.log(response.data);
+      console.log(response.data.data.InsertedID);
+      alert(`${response.data.data.InsertedID}`);
+      return setfirst("");
+    }
+  };
+
+  const handleAllUsers = async () => {
+    const response = await connecUrl.get("/users");
+    if (response.data) {
+      console.log(response.data);
+      setAllUsers(response.data.data);
+    }
   };
   return (
-    <>
-      <h1>Hello Vite!</h1>
-      <button onClick={() => handleButtonClick()}>Click me</button>
-    </>
+    <div>
+      <form onSubmit={(e) => handleButtonClick(e)}>
+        <input
+          name="name"
+          value={first}
+          onChange={(e) => setfirst(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <button onClick={() => handleAllUsers()}>All users</button>
+      <ul>
+        {allUsers.map((user) => (
+          console.log(user),
+          <div key={user.InsertedID}>{user.name}</div>
+        ))}
+      </ul>
+    </div>
   );
 }
 
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/gars150387/go-react-crud-project.git/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -14,13 +19,46 @@ func main() {
 		port = "3000"
 	}
 	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://gustadolrodsant:UmX8JlbnVxuwSTnG@trading-bot.xefj3.mongodb.net/gomongo"))
+	if err != nil {
+		panic(err)
+	}
 	app.Use((cors.New()))
-	app.Static("/", "./client/dist") 
+	app.Static("/", "./client/dist")
 	app.Get("/users", func(c *fiber.Ctx) error {
+		var users []models.User
+		coll := client.Database("gomongo").Collection("users")
+		result, err := coll.Find(context.TODO(), bson.D{})
+		if err != nil {
+			return err
+		}
+		for result.Next(context.TODO()) {
+			var user models.User
+			err := result.Decode(&user)
+			if err != nil {
+				panic(err)
+			}
+			users = append(users, user)
+		}
 		return c.JSON(&fiber.Map{
-			"data": "usuarios desde el backend",
+			"ok":   true,
+			"data": users,
+		})
+	})
+	app.Post("/user", func(c *fiber.Ctx) error {
+		var user models.User
+		c.BodyParser(&user)
+
+		coll := client.Database("gomongo").Collection("users")
+		result, erro := coll.InsertOne(context.TODO(), bson.D{{"name", user.Name}})
+
+		if erro != nil {
+			return err
+		}
+		return c.JSON(&fiber.Map{
+			"data": result,
 		})
 	})
 	app.Listen(":" + port)
-	fmt.Println("Server running on port 3000")
+	fmt.Println("Server running on port " + port)
 }
